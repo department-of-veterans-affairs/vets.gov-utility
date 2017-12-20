f455935297960fe2e9c731df40d9757e848ea307
Robert Fairhead <bob@adhocteam.us>
2017-12-20 09:25:42 -0600

    Switch to new set of dashboards

--- dashshot/index.js
+++ dashshot/index.js
@@ -25,19 +25,19 @@ async function create_pdf(team, dashes) {
   await browser.close();
 }
 
-const dashes = {"kudos": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbcUNSbTlnWjZaVzA/page/IBLI", file: "kudos-1.pdf"},
-                          {url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbcUNSbTlnWjZaVzA/page/GELI", file: "kudos-2.pdf"}],
-                "rainbows": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbR3o4bDBfLTZFSmM/page/IBLI", file: "rainbows-1.pdf"},
-                             {url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbR3o4bDBfLTZFSmM/page/GELI", file: "rainbows-2.pdf"}],
-                "unicorns": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/13_08yLsP1ncNgyWr5Ug__-fXEBCVjRs2/page/GELI", file: "unicorns.pdf"}],
-                "nebula": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1hg4NB2RGJl4Hu-4ZUro6v2fdPgndpJDA/page/9mgG", file: "nebula-1.pdf"},
-                           {url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1hg4NB2RGJl4Hu-4ZUro6v2fdPgndpJDA/page/ueaI", file: "nebula-2.pdf"}],
-                "exec": [{ url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbODZBNmphUEhhMWM/page/9mgG", file: "exec-1.pdf"},
-                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbODZBNmphUEhhMWM/page/X94K", file: "exec-2.pdf"},
-                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbODZBNmphUEhhMWM/page/XajK", file: "exec-3.pdf"},
-                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbODZBNmphUEhhMWM/page/ueaI", file: "exec-4.pdf"},
-                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbODZBNmphUEhhMWM/page/j24K", file: "exec-5.pdf"},
-                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/0B-eryOVvbpHbODZBNmphUEhhMWM/page/TXxK", file: "exec-6.pdf"}]
+const dashes = {"kudos": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1Jtp5YJsxzdRgApBIbcDMx5420U1VsKVt/page/IBLI", file: "kudos-1.pdf"},
+                          {url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1Jtp5YJsxzdRgApBIbcDMx5420U1VsKVt/page/GELI", file: "kudos-2.pdf"}],
+                "rainbows": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1OfD78MumK5roK7iBByiQv2mv271sKeac/page/IBLI", file: "rainbows-1.pdf"},
+                             {url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1OfD78MumK5roK7iBByiQv2mv271sKeac/page/GELI", file: "rainbows-2.pdf"}],
+                "unicorns": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/14Gv0B2uVXgK8_U4IPnuHP43ZMDA0qJft/page/GELI", file: "unicorns.pdf"}],
+                "nebula": [{url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1xsr2zq9vG8LNw0qwCG7bXX2L6APtGIR-/page/9mgG", file: "nebula-1.pdf"},
+                           {url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1xsr2zq9vG8LNw0qwCG7bXX2L6APtGIR-/page/ueaI", file: "nebula-2.pdf"}],
+                "exec": [{ url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1GX0vKX93p84bqLK3sL5Bv7JusOay6jl7/page/9mgG", file: "exec-1.pdf"},
+                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1GX0vKX93p84bqLK3sL5Bv7JusOay6jl7/page/X94K", file: "exec-2.pdf"},
+                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1GX0vKX93p84bqLK3sL5Bv7JusOay6jl7/page/XajK", file: "exec-3.pdf"},
+                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1GX0vKX93p84bqLK3sL5Bv7JusOay6jl7/page/ueaI", file: "exec-4.pdf"},
+                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1GX0vKX93p84bqLK3sL5Bv7JusOay6jl7/page/j24K", file: "exec-5.pdf"},
+                         { url: "https://datastudio.google.com/org/oXPY3GFFQwaHnjNHpFLyFg/reporting/1GX0vKX93p84bqLK3sL5Bv7JusOay6jl7/page/TXxK", file: "exec-6.pdf"}]
 };
 
 (async () => { for (let team in dashes) {
