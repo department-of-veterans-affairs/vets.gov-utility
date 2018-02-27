fa73566ead0df9b48db107f1eb45c0e1b67dc4c3
Derek <derekhall77@gmail.com>
2018-02-27 11:16:07 -0500

    update specs to pull multiple pages of data when necessary; dropped the spec that expects all data in a single request

--- facilities/validation/gis_helper.rb
+++ facilities/validation/gis_helper.rb
@@ -4,14 +4,6 @@ require 'json'
 
 # Spec helper to fetch GIS data and metadata
 class GISHelper
-  ALL_PARAMS = {
-    where: '1=1',
-    inSR: 4326,
-    outSR: 4326,
-    returnGeometry: true,
-    outFields: '*',
-    f: 'json'
-  }.freeze
 
   METADATA_PARAMS = {
     f: 'json'
@@ -22,13 +14,25 @@ class GISHelper
                      vha: 'VHA_Facilities',
                      vc: 'VHA_VetCenters'
                     }.freeze
-  def self.get_all(url)
+
+  def self.get_all(url,max_record_count)
     conn = Faraday.new(url: url) do |c|
       # c.response :logger
       c.adapter Faraday.default_adapter
     end
-    response = conn.get url, ALL_PARAMS
-    JSON.parse(response.body)
+    data = JSON.parse conn.get(url, params).body
+    count = data['features'].length
+    query_count = 1
+
+    #fetch additional pages when applicable
+    while count == max_record_count
+      response = conn.get(url, params.merge(resultOffset:(query_count * max_record_count)))
+      additional_data = JSON.parse(response.body)
+      data['features'] += additional_data['features']
+      count = additional_data['features'].length
+      query_count += 1
+    end
+    data
   end
 
   def self.get_metadata(url)
@@ -43,5 +47,16 @@ class GISHelper
   def self.get_layer_url(type)
     stage = ENV['USE_PROD_GIS'] ? nil : '_stage'
     "https://services3.arcgis.com/aqgBd3l68G8hEFFE/ArcGIS/rest/services/#{FACILITY_TYPES[type]}#{stage}/FeatureServer/0"
+  end
+
+  def self.params
+    {
+      where: '1=1',
+      inSR: 4326,
+      outSR: 4326,
+      returnGeometry: true,
+      outFields: '*',
+      f: 'json'
+    }
   end
 end
--- facilities/validation/vha_data_spec.rb
+++ facilities/validation/vha_data_spec.rb
@@ -85,14 +85,7 @@ describe 'VHA Data' do
     query_url = [layer_url, 'query'].join('/')
 
     @metadata = GISHelper.get_metadata(layer_url)
-    @data = GISHelper.get_all(query_url)
-  end
-
-  describe 'count' do
-    it 'returns less than maxRecordCount results' do
-      max = @metadata['maxRecordCount']
-      expect(@data['features'].length).to be < max, 'Max record count reached, spec needs to fetch in batches'
-    end
+    @data = GISHelper.get_all(query_url, @metadata['maxRecordCount'])
   end
 
   describe 'station numbers' do
